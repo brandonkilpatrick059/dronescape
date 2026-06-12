0b0e7f968d62408710bbfdc68f96c4984ab2efb9
Brandon Kilpatrick <brandonkilpatrick059@gmail.com>
2026-06-11 22:44:32 -0500

    added planet

---
+++ scripts/planet.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+var origin_point : Vector2
+
+func _ready() -> void:
+	origin_point = position
+
+func _physics_process(delta: float) -> void:
+	var speed : float = 0.006
+
+	position = position + Vector2(speed*2,-speed)
+	if(position.distance_to(origin_point) > 1280):
+		position = origin_point
--- scripts/skybox_manager.gd
+++ scripts/skybox_manager.gd
@@ -3,15 +3,17 @@ extends Parallax2D
 @onready var sky_gradient : Sprite2D = $sky_gradient
 @onready var ground_gradient : Sprite2D = $ground_gradient
 @onready var starfield : Sprite2D = $starfield
+@onready var planet : Sprite2D = $planet
 
 var interpolation_gradient = Gradient.new()
 var ground_interpolation_gradient = Gradient.new()
 var starfield_interpolation_gradient = Gradient.new()
+var planet_interpolation_gradient = Gradient.new()
 
 var hour_length_seconds : float = 10.0
 var timer_clock := Timer.new()
 
-var clock = 19
+var clock = 17
 
 var colors := {
 	0.0:      Color(0.314, 0.322, 0.467), #12:00am
@@ -94,6 +96,33 @@ var starfield_colors := {
 	1.0:      Color(0.8, 0.8, 0.8), #11
 }
 
+var planet_colors := {
+	0.0:      Color(1, 1, 1, 1), #12:00am
+	0.041666: Color(1, 1, 1, 1), #1:00
+	0.083332: Color(1, 1, 1, 1), #2
+	0.124998: Color(1, 1, 1, 1), #3
+	0.166664: Color(1, 1, 1, 1), #4
+	0.20833:  Color(1, 1, 1, 0.4), #5
+	0.249996: Color(1, 1, 1, 0.2), #6
+	0.291662: Color(1, 1, 1, 0.2), #7
+	0.333328: Color(1, 1, 1, 0.1), #8
+	0.374994: Color(1, 1, 1, 0.1), #9
+	0.41666:  Color(1, 1, 1, 0.1), #10
+	0.458326: Color(1, 1, 1, 0.1), #11
+	0.499992: Color(1, 1, 1, 0.1), #12:00pm
+	0.541658: Color(1, 1, 1, 0.1), #1:00pm
+	0.583324: Color(1, 1, 1, 0.1), #2
+	0.62499:  Color(1, 1, 1, 0.2), #3
+	0.666656: Color(1, 1, 1, 0.4), #4
+	0.708322: Color(1, 1, 1, 1), #5
+	0.749988: Color(1, 1, 1, 1), #6
+	0.791654: Color(1, 1, 1, 1),#7
+	0.83332:  Color(1, 1, 1, 1), #8
+	0.874986: Color(1, 1, 1, 1), #9
+	0.916652: Color(1, 1, 1, 1), #10
+	1.0:      Color(1, 1, 1, 1), #11
+}
+
 func get_time_as_ratio_of_full_day() -> float:
 	var ratio = 0.0
 	ratio = (clock+1.0)/24.0
@@ -116,6 +145,8 @@ func _ready() -> void:
 	ground_interpolation_gradient.colors = ground_colors.values()
 	starfield_interpolation_gradient.offsets = starfield_colors.keys()
 	starfield_interpolation_gradient.colors = starfield_colors.values()
+	planet_interpolation_gradient.offsets = planet_colors.keys()
+	planet_interpolation_gradient.colors = planet_colors.values()
 
 func _physics_process(delta: float) -> void:
 	if(timer_clock.is_stopped()):
@@ -125,3 +156,4 @@ func _physics_process(delta: float) -> void:
 	sky_gradient.modulate = interpolation_gradient.sample(day_ratio)
 	ground_gradient.modulate = ground_interpolation_gradient.sample(day_ratio)
 	starfield.modulate = starfield_interpolation_gradient.sample(day_ratio)
+	planet.modulate = planet_interpolation_gradient.sample(day_ratio)
